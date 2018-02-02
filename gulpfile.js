c98b58a7cb259a801266efc7df184d2b4a3142a8
Michael Ekstrand <md@ekstrandom.net>
2018-02-02 15:24:07 -0700

    support downloading the hcibib

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const fs = require('fs-extra');
+
+const hcibib = require('./lib/hcibib');
+
+const dataDir = 'data';
+
+module.exports.listBibFiles = async function listBibFiles() {
+    let files = await hcibib.listBibFiles();
+    for (let fn of files) {
+        console.log('found file %s', fn);
+    }
+};
+module.exports.download = async function download() {
+    let mdr = await fs.ensureDir(dataDir);
+    return hcibib.downloadFiles(dataDir);
+};
