3b27f6ed7ccd8cc82dc503f5feaa8fc8161c084e
Michael Ekstrand <md@ekstrandom.net>
2020-03-27 14:48:27 -0600

    Fix SQL queries & clean up errors

--- src/main/java/edu/boisestate/cs410/articles/ArticleShell.java
+++ src/main/java/edu/boisestate/cs410/articles/ArticleShell.java
@@ -37,20 +37,12 @@ public class ArticleShell implements Runnable {
         }
     }
 
-    /**
-     * We can run
-     * <pre>
-     *     conference-top-authors CHI
-     * </pre>
-     * @param series
-     * @throws SQLException
-     */
     @Command
     public void conferenceTopAuthors(String series) throws SQLException {
         String query = "SELECT author_id, author_name, COUNT(article_id) AS article_count" +
                 " FROM author JOIN article_author USING (author_id)" +
                 " JOIN article USING (article_id)" +
-                " JOIN proceedings USING (proc_id)" +
+                " JOIN proceedings USING (pub_id)" +
                 " JOIN conf_series USING (cs_id)" +
                 " WHERE cs_hb_key = ?" +
                 " GROUP BY author_id" +
@@ -68,32 +60,6 @@ public class ArticleShell implements Runnable {
                 }
             }
         }
-    }
-
-    /**
-     * SQL injection!
-     * @throws SQLException
-     */
-    @Command
-    public void badConferenceTopAuthors(String series) throws SQLException {
-        String query = "SELECT author_id, author_name, COUNT(article_id) AS article_count" +
-                " FROM author JOIN article_author USING (author_id)" +
-                " JOIN article USING (article_id)" +
-                " JOIN proceedings USING (proc_id)" +
-                " JOIN conf_series USING (cs_id)" +
-                " WHERE cs_hb_key = '" + series + "'" +
-                " GROUP BY author_id" +
-                " ORDER BY article_count DESC LIMIT 10";
-        System.out.format("Top Authors in %s by Publication Count:%n", series);
-        try (Statement stmt = db.createStatement();
-             ResultSet rs = stmt.executeQuery(query)) {
-            while (rs.next()) {
-                String name = rs.getString("author_name");
-                int count = rs.getInt("article_count");
-                System.out.format("  %s (with %d pubs)\n", name, count);
-            }
-        }
-
     }
 
     /**
